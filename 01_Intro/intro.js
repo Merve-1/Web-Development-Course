de785d705d799e1b82d478784831e79d90e4eb98
malazab40444 <malazab404@gmail.com>
2026-05-03 09:30:12 +0300

    intro

---
+++ 01_Intro/intro.js
@@ -0,0 +1,64 @@
+var x = document.querySelector('h3');
+console.log(x);
+
+window.alert("Hello World");
+document.write("Hello World from doc write");
+window.prompt("Enter your name: ");
+
+console.log(1+1);
+console.log("Hello");
+
+x='ahmed'
+console.log(x)
+y='mariam'
+console.log(y)
+
+var name = 'Mohammed'
+console.log(name)
+
+//declaration 
+var z;
+//assignment 
+z=5;
+
+console.log(z);
+//initialization 
+var k = 3;
+console.log(k);
+
+//primitive datatype number / string / null/ undefined/ bool 
+//non-primitive datatype functions array objects 
+
+var  role = "admin"
+console.log(typeof role);
+
+var status = false;
+console.log(status)
+
+var placeholder;
+console.log(placeholder);
+
+var test = null;
+console.log(test);
+
+var list =[1,2,3,4,5];
+console.log(list);
+
+var list2 =["Ali", "Sarah", "Mohammed"];
+console.log(list2);
+
+
+var list3 =["Ali", "Sarah", 5];
+console.log(list3);
+console.log(typeof list3);
+
+
+var person = {
+    //key: value,
+    name: "Ahmed",
+    age: 23,
+    address: "Alex"
+}
+
+console.log(person);
+console.log(typeof person)
